0b12aa324647d844ea6802a725f1b499dd8a36c7
Meela Maniteja <126406121+Maniteja03@users.noreply.github.com>
2025-05-11 08:26:19 +0530

    Add files via upload

---
+++ configServer/configServer/src/test/java/com/ctg/congif/server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctg.congif.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
